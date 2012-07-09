91287b22ec0baf2691ac0e5dc2d5b6fed713e1e4
Allen <haochen_al@sina.com>
2012-07-09 18:25:51 +0800

    building model

--- nbg/models.py
+++ nbg/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+#Building Model
+class Building(models.Model):
+    name = models.CharField(max_length=30)
+    university = models.ForeignKey(University)
+    latitude = models.DecimalField(max_digits=10, decimal_places=6)
+    longitude = models.DecimalField(max_digits=10, decimal_places=6)
