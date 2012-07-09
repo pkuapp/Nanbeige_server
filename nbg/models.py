c41264bca16ad993c0af6c019d31af2e567d33d8
Allen <haochen_al@sina.com>
2012-07-09 21:32:37 +0800

    update model.

--- nbg/models.py
+++ nbg/models.py
@@ -1,18 +1,21 @@
 from django.db import models
 from django.conf import settings
 
-class Building(models.Model):
-    name = models.CharField(max_length=30)
-    university = models.ForeignKey(University)
-    latitude = models.DecimalField(max_digits=10, decimal_places=6)
-    longitude = models.DecimalField(max_digits=10, decimal_places=6)
-
 class App(models.Model):
     version_android = models.CharField(max_length=30)
     version_ios = models.CharField(max_length=30)
 
 class University(models.Model):
     name = models.CharField(max_length=200)
+
+class Course(models.Model):
+    name = models.CharField(max_length=200)
+    
+class Building(models.Model):
+    name = models.CharField(max_length=30)
+    university = models.ForeignKey(University)
+    latitude = models.DecimalField(max_digits=10, decimal_places=6)
+    longitude = models.DecimalField(max_digits=10, decimal_places=6)
 
 class Classroom(models.Model):
     name = models.CharField(max_length=30)
@@ -23,5 +26,3 @@ class ClassroomAvailability(models.Model):
     date = models.DateField()
     isAvailable = models.BooleanField()
 
-class Course(models.Model):
-    name = models.CharField(max_length=200)
