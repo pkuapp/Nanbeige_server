319102fc62866bed0beed35dd00c761d29ef89f2
Allen <haochen_al@sina.com>
2012-07-10 22:38:53 +0800

    update views_app.py

--- nbg/views_app.py
+++ nbg/views_app.py
@@ -1,5 +1,15 @@
+# -*- coding:UTF-8 -*-
 from django.http import HttpResponse
 from django.utils import simplejson
 
 def version_api(request):
-	return HttpResponse(simplejson.dumps({'version':'9'}),mimetype='application/json')
+    return HttpResponse(simplejson.dumps({'version':'9'}),mimetype='application/json')
+
+def version_android(request):
+    return HttpResponse(simplejson.dumps({'stable':'0.618', 'beta':'0.619'}),mimetype='application/json')
+
+def iOS_version(request):
+    return HttpResponse(simplejson.dumps({'stable':'1.0','beta':'1.1'}),mimetype='application/json')
+
+def notice(request):
+    return HttpResponse(simplejson.dumps({'time':'2012-04-01 00:00:00','content':'由于不可抗拒的因素，颐和园路5号将不再提供服务。'}),mimetype='application/json')
