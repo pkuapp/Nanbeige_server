eb26617a64c86042c0f364871e0399be25cade13
Xhacker Liu <liu.dongyuan@gmail.com>
2012-07-10 20:05:13 +0800

    update settings.

--- settings.py
+++ settings.py
@@ -121,7 +121,7 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    'Nanbeige_server.nbg',
+    'nbg',
 )
 
 # A sample logging configuration. The only tangible logging
