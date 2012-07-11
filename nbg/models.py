0e4502ac7617e920fffd98d8f7379279465c605c
Xhacker Liu <liu.dongyuan@gmail.com>
2012-07-11 19:40:32 +0800

    update model.

--- nbg/models.py
+++ nbg/models.py
@@ -6,12 +6,33 @@ class App(models.Model):
     version_android_stable = models.CharField(max_length=30)
     version_ios_beta = models.CharField(max_length=30)
     version_ios_stable = models.CharField(max_length=30)
+    notice = models.TextField()
 
 class University(models.Model):
     name = models.CharField(max_length=200)
+    english_name = models.CharField(max_length=200)
+    latitude = models.DecimalField(max_digits=10, decimal_places=6)
+    longitude = models.DecimalField(max_digits=10, decimal_places=6)
 
 class Course(models.Model):
     name = models.CharField(max_length=200)
+    original_id = models.CharField(max_length=100)
+    credit = models.DecimalField(max_digits=3, decimal_places=1)
+    weeks = models.CommaSeparatedIntegerField(max_length=100)
+
+class Lesson(models.Model):
+    day = models.SmallIntegerField()
+    start = models.SmallIntegerField()
+    end = models.SmallIntegerField()
+    location = models.CharField(max_length=200)
+    course = models.ForeignKey(Course)
+
+class Teacher(models.Model):
+    name = models.CharField(max_length=50)
+    course = models.ForeignKey(Course)
+
+class Ta(models.Model):
+    name = models.CharField(max_length=50)
 
 class Building(models.Model):
     name = models.CharField(max_length=30)
