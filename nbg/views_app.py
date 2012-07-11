466290eca09b984a01db4ae9fb20a591d3d12981
Allen <haochen_al@sina.com>
2012-07-11 20:20:13 +0800

    update views_app.py

--- nbg/views_app.py
+++ nbg/views_app.py
@@ -1,15 +1,23 @@
 # -*- coding:UTF-8 -*-
 from django.http import HttpResponse
 from django.utils import simplejson
+from django.shortcuts import *
+from django.conf import settings
+from nbg.models import App
+from datetime import datetime
 
 def version_api(request):
-    return HttpResponse(simplejson.dumps({'version':'9'}), mimetype='application/json')
+    return HttpResponse(simplejson.dumps({'version':settings.API_VERSION}), mimetype='application/json')
 
 def version_android(request):
-    return HttpResponse(simplejson.dumps({'stable':'0.618', 'beta':'0.619'}), mimetype='application/json')
+    app_obj = App.objects.all()[0]
+    return HttpResponse(simplejson.dumps({'stable': app_obj.version_android_stable, 'beta': app_obj.version_android_beta}), mimetype='application/json')
 
 def version_ios(request):
-    return HttpResponse(simplejson.dumps({'stable':'1.0', 'beta':'1.1'}), mimetype='application/json')
+    app_obj = App.objects.all()[0]
+    return HttpResponse(simplejson.dumps({'stable':app_obj.version_ios_stable ,'beta':app_obj.version_ios_beta}), mimetype='application/json')
 
 def notice(request):
-    return HttpResponse(simplejson.dumps({'time':'2012-04-01 00:00:00', 'content':'由于不可抗拒的因素，颐和园路5号将不再提供服务。'}), mimetype='application/json')
+    app_obj = App.objects.all()[0]
+    notice_time = str(app_obj.notice_time)
+    return HttpResponse(simplejson.dumps({'time':notice_time, 'content':app_obj.notice_content}), mimetype='application/json')
--- nbg/views_course.py
+++ nbg/views_course.py
@@ -2,5 +2,3 @@
 from django.http import HttpResponse
 from django.utils import simplejson
 
-def homework_list(request):
-    return HttpResponse(simplejson.dumps({'xxx':'yyy'}),mimetype='application/json')
