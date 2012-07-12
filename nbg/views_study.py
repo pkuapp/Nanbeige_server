09bd1bac9c26fb30b5074e6e7e8020a8233bd998
Allen <haochen_al@sina.com>
2012-07-12 22:19:01 +0800

    update view_study.py

--- nbg/views_study.py
+++ nbg/views_study.py
@@ -3,20 +3,29 @@ from django.http import HttpResponse
 from django.utils import simplejson
 from django.shortcuts import *
 from nbg.models import *
+from datetime import datetime
 
 def building_list(request):
     university_id = request.GET.get('university_id', None)
     if university_id:
         try:
-            building_array = Building.objects.filter(university=university_id).values()
-            response = [ {'id' : item['id'], 'name': item['name'], 'latitude': float(item['latitude']), 'longitude': float(item['longitude']) } for item in building_array]
+            building_values = Building.objects.filter(university=university_id).values()
+            response = [ {'id' : item['id'], 'name': item['name'], 'latitude': float(item['latitude']), 'longitude': float(item['longitude']) } for item in building_values]
             return HttpResponse(simplejson.dumps(response,sort_keys=True),mimetype='application/json')
         except:
-            return HttpResponse(simplejson.dumps({'error': '没有找到这个教学楼'}),mimetype='application/json')
+            return HttpResponse(simplejson.dumps({'error': '尚无数据'}),mimetype='application/json')
     else:
         return HttpResponse(simplejson.dumps({'error': '缺少必要的参数'}),mimetype='application/json')
 
-def room_list(request):
-    #building_id = request.GET.
-    #room_array = Classroom.objects.
-    return HttpResponse(simplejson.dumps([{'id':'101','name':'room 101','available':['true or false?']},{'id':'102','name':'room 102','available':['true or false?']},'...']),mimetype='application/json')
+def room_list(request, offset):
+    building_id = int(offset)
+    if building_id and request.GET.get('date', None):
+        pdate = datetime.strptime(request.GET.get('date', None), '%Y%m%d')
+        try:
+            room_values = Classroom.objects.filter(building=building_id).values()
+            response = [ {'id' : item['id'], 'name': item['name'], 'availability': ClassroomAvailability.objects.filter(classroom=item['id'], date=pdate).values()[0]['availability']} for item in room_values]
+            return HttpResponse(simplejson.dumps(response,sort_keys=True),mimetype='application/json')
+        except:
+            return HttpResponse(simplejson.dumps({'error': '尚无数据'}),mimetype='application/json')
+    else:
+        return HttpResponse(simplejson.dumps({'error': '缺少必要的参数'}),mimetype='application/json')
--- urls.py
+++ urls.py
@@ -23,24 +23,24 @@ urlpatterns = patterns('',
     (r'^user/login/weibo/$', 'nbg.views_user.login_weibo'),
 
     (r'^university/list/$', 'nbg.views_university.university_list'),
-    (r'^university/\d+/$', 'nbg.views_university.detail'),
+    (r'^university/(\d+)/$', 'nbg.views_university.detail'),
 
     (r'^course/list/$', 'nbg.views_course.list'),
     (r'^course/homework/list/$', 'nbg.views_course.homework_list'),
-    (r'^course/homework/\d+/finish/$', 'nbg.views_course.homework_finish'),
-    (r'^course /homework/\d+/delete/$', 'nbg.views_course.homework_delete'),
-    (r'^course/homework/\d+/modify/$', 'nbg.views_course.homework_modify'),
+    (r'^course/homework/(\d+)/finish/$', 'nbg.views_course.homework_finish'),
+    (r'^course /homework/(\d+)/delete/$', 'nbg.views_course.homework_delete'),
+    (r'^course/homework/(\d+)/modify/$', 'nbg.views_course.homework_modify'),
     (r'^course/homework/add/$', 'nbg.views_course.homework_add'),
-    (r'^course/\d+/comment/add/$', 'nbg.views_course.comment_add'),
-    (r'^course/\d+/comment/list/$', 'nbg.views_course.comment_list'),
+    (r'^course/(\d+)/comment/add/$', 'nbg.views_course.comment_add'),
+    (r'^course/(\d+)/comment/list/$', 'nbg.views_course.comment_list'),
 
     (r'^comment/address/$', 'nbg.views_comment.address'),
 
     (r'^study/building/list/$', 'nbg.views_study.building_list'),
-    (r'^study/building/\d+/room/list/$', 'nbg/views_study.room_list'),
+    (r'^study/building/(\d+)/room/list/$', 'nbg.views_study.room_list'),
 
     (r'^event/address/$', 'nbg.views_event.address'),
 
-    (r'^wiki/list/\d+/$', 'nbg.views_wiki.wiki_list'),
-    (r'^wiki/node/\d+/$', 'nbg.views_wiki.wiki_node'),
+    (r'^wiki/list/(\d+)/$', 'nbg.views_wiki.wiki_list'),
+    (r'^wiki/node/(\d+)/$', 'nbg.views_wiki.wiki_node'),
 )
