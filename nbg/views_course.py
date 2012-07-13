c56b6928903b2844003874aa0a5885b4bf4b087d
Xhacker Liu <liu.dongyuan@gmail.com>
2012-07-13 15:23:15 +0800

    "homework" => "assignment".

--- nbg/views_course.py
+++ nbg/views_course.py
@@ -2,3 +2,5 @@
 from django.http import HttpResponse
 from django.utils import simplejson
 
+def assignment_list(request):
+    return HttpResponse(simplejson.dumps({'xxx':'yyy'}),mimetype='application/json')
