8432a10e1d87b40ae396005a2fff47fa05735e89
Xhacker Liu <liu.dongyuan@gmail.com>
2012-07-13 20:06:05 +0800

    add event model. (experimental)

--- nbg/views_course.py
+++ nbg/views_course.py
@@ -3,5 +3,8 @@
 from django.http import HttpResponse
 from django.utils import simplejson
 
+def course_list(request):
+	pass
+
 def assignment_list(request):
-    return HttpResponse(simplejson.dumps({'xxx':'yyy'}),mimetype='application/json')
+    return HttpResponse(simplejson.dumps({'xxx': 'yyy'}),mimetype='application/json')
