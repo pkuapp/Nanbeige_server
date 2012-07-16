33463d7be60416a966538df8a990a15f32c7c294
Xhacker Liu <liu.dongyuan@gmail.com>
2012-07-16 16:29:43 +0800

    update course view. not finished yet.

--- nbg/views_course.py
+++ nbg/views_course.py
@@ -4,7 +4,14 @@ from django.http import HttpResponse
 from django.utils import simplejson
 
 def course_list(request):
-	pass
+    course_values = Course.objects.values()
+    response = [{
+        'id': item.id,
+        'orig_id': item.original_id',
+        'name': item.name,
+        'credit': item.credit,
+    } for item in course_values]
+    return HttpResponse(simplejson.dumps(response), mimetype='application/json')
 
 def assignment_list(request):
     return HttpResponse(simplejson.dumps({'xxx': 'yyy'}),mimetype='application/json')
