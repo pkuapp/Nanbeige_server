02c1ec3b5e80edbbba252c9f18fb2a1c1d89d599
Xhacker Liu <liu.dongyuan@gmail.com>
2012-07-25 16:39:07 +0800

    better handle error in building_list.

--- nbg/views_study.py
+++ nbg/views_study.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 from django.utils import simplejson
 from django.shortcuts import *
 from nbg.models import *
@@ -8,15 +8,20 @@ from datetime import datetime
 
 def building_list(request):
     university_id = request.GET.get('university_id', None)
-    if university_id:
-        try:
-            building_values = Building.objects.filter(university=university_id).values()
-            response = [ {'id' : item['id'], 'name': item['name'], 'latitude': float(item['latitude']), 'longitude': float(item['longitude']) } for item in building_values]
-            return HttpResponse(simplejson.dumps(response,sort_keys=True), mimetype='application/json')
-        except:
-            return HttpResponse(simplejson.dumps({'error': '尚无数据'}), mimetype='application/json')
-    else:
-        return HttpResponse(simplejson.dumps({'error': '缺少必要的参数'}), mimetype='application/json')
+
+    try:
+        university = University.objects.get(pk=university_id)
+    except:
+        return HttpResponseNotFound(simplejson.dumps({'error': '学校不存在。'}), mimetype='application/json')
+
+    buildings = university.building_set.all()
+    response = [{
+        'id': item.id,
+        'name': item.name,
+        'latitude': float(item.latitude),
+        'longitude': float(item.longitude),
+    } for item in buildings]
+    return HttpResponse(simplejson.dumps(response), mimetype='application/json')
 
 def room_list(request, offset):
     building_id = int(offset)
