698a11eae5ebd096bb82d05dfb86b5460f379ca3
Xhacker Liu <liu.dongyuan@gmail.com>
2012-07-25 18:27:49 +0800

    minor settings update.

--- settings.py
+++ settings.py
@@ -24,27 +24,12 @@ DATABASES = {
     }
 }
 
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# On Unix systems, a value of None will cause Django to use the same
-# timezone as the operating system.
-# If running in a Windows environment this must be set to the same as your
-# system time zone.
 TIME_ZONE = 'Asia/Shanghai'
-
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = 'zh-CN'
 
 SITE_ID = 1
 
-# If you set this to False, Django will make some optimizations so as not
-# to load the internationalization machinery.
-USE_I18N = True
-
-# If you set this to False, Django will not format dates, numbers and
-# calendars according to the current locale
+USE_I18N = False
 USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
@@ -86,7 +71,6 @@ STATICFILES_FINDERS = (
 #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
-# Make this unique, and don't share it with anybody.
 SECRET_KEY = '3o1rcvdh^4wewz9g4co9xph*5s*pty8svq&q*prn3m6d$x)gx^'
 
 # List of callables that know how to import templates from various sources.
