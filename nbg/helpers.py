edcd68d35ad765d287c5fe7d8129355c379210d4
ZeR0-Wu <thelongestmailaccounteverexist@gmail.com>
2012-07-30 15:32:01 +0800

    add decorator auth_required to helpers as a custom implement of @login_required

--- nbg/helpers.py
+++ nbg/helpers.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from string import split
 from django.utils.simplejson import dumps
 from django.http import HttpResponse
@@ -15,4 +16,12 @@ def json_response(func):
     def inner(request, *args, **kwargs):
         dict_response = func(request, *args, **kwargs)
         return HttpResponse(dumps(dict_response), mimetype="application/json")
+    return inner
+
+def auth_required(func):
+    def inner(request, *args, **kwargs):
+        if request.user.is_authenticated():
+            return func(request, *args, **kwargs)
+        else:
+            return HttpResponse(dumps({'error': '请先登录。'}))
     return inner
