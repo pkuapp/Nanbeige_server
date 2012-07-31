202ccba789996a56642e51f6b75af28dce980820
ZeR0-Wu <thelongestmailaccounteverexist@gmail.com>
2012-07-31 17:41:19 +0800

    .

--- nbg/tests.py
+++ nbg/tests.py
@@ -118,6 +118,7 @@ class LogicTest(TestCase):
             })
         assert response.status_code == 200
         response = self.c.post(urlr)
+        print response.content
         assert json.loads(response.content)[-1]['content'] is u'请吴昊天吃金钱豹'
 
         response = self.c.post('/course/%s/delete/' % (assignment_id))
