d7893f0e05bd4a8faec7be42c44ecd96f045ba74
Xhacker Liu <liu.dongyuan@gmail.com>
2012-07-31 14:06:31 +0800

    implement event query.

--- nbg/views_event.py
+++ nbg/views_event.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from datetime import datetime
+from django.db.models import Q
+from django.core.exceptions import ValidationError
 from nbg.models import Event, EventCategory
 from nbg.helpers import json_response, auth_required
 
@@ -8,22 +10,24 @@ from nbg.helpers import json_response, auth_required
 def query(request):
     keyword =request.GET.get('keyword', None)
     category_id = request.GET.get('category_id', None)
-    after = request.GET.get('after', None)
+    after = request.GET.get('after', datetime.now())
     before = request.GET.get('before', None)
     start = int(request.GET.get('start', 0))
 
-    event_objs = Event.objects.all()
+    try:
+        event_objs = Event.objects.filter(time__gte=after)
+    except ValidationError:
+        return {'error': 'after 日期格式错误。'}
     if keyword:
-        event_objs = event_objs.filter(title__contains=keyword)
+        event_objs = event_objs.filter(Q(title__contains=keyword)|Q(subtitle__contains=keyword))
     if category_id:
         event_objs = event_objs.filter(category=category_id)
-    if after:
-        event_objs = event_objs.filter(time__gte=datetime.fromtimestamp(float(after)))
-    else:
-        event_objs = event_objs.filter(time__gte=datetime.now())
     if before:
-        event_objs = event_objs.filter(time__lte=datetime.fromtimestamp(float(before)))
-    event_objs = event_objs[start:10]
+        try:
+            event_objs = event_objs.filter(time__lte=before)
+        except ValidationError:
+            return {'error': 'before 日期格式错误。'}
+    event_objs = event_objs[start:start+10]
 
     response = [{
         'id': item.pk,
