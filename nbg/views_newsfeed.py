f38655354ea43ee7a5bdd0e8b286053d6bebe809
ZeR0-Wu <thelongestmailaccounteverexist@gmail.com>
2012-09-07 18:29:02 +0800

    .

--- nbg/views_newsfeed.py
+++ nbg/views_newsfeed.py
@@ -11,14 +11,14 @@ def newsfeed(request):
     after = request.GET.get('after', parse_datetime('2000-1-1 00:00:00'))
     newsfeed_list = cache.get(request.session.session_key+'_newsfeed')
     if newsfeed_list:
-        sub_list ＝ newsfeed_list.filter(time__gte=after).order_by('time')
+        sub_list = newsfeed_list.filter(time__gte=after).order_by('time')
     else:
         newsfeed_list = list()
         user_profile = request.user.get_profile()
-        course_id_list = user_profile.courses.value_list('id', flat=True).order_by('id')
-        newsfeed_list = NewsFeed.objects.filter(id__in=course_id_list, ref_model='Course')
+        courses_id_list = user_profile.courses.all().values_list('id', flat=True).order_by('id')
+        newsfeed_list = NewsFeed.objects.filter(id__in=courses_id_list, ref_model='Course')
         cache.set(request.session.session_key+'_newsfeed', newsfeed_list)
-        sub_list ＝ newsfeed_list.filter(time__gte=after).order_by('time')
+        sub_list = newsfeed_list.filter(time__gte=after).order_by('time')
 
     ret = [{
             'news_type': NewsFeed.NEWS_TYPE_CHOICES[x.news_type][1],
