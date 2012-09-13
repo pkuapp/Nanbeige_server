435b62a41eb1c107599b21ea1257b5dc05b4a1ab
Xhacker Liu <liu.dongyuan@gmail.com>
2012-09-13 11:58:44 +0800

    format tweaks.

--- spider/grabbers/grabber_7.py
+++ spider/grabbers/grabber_7.py
@@ -1,18 +1,14 @@
-
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os
-import yaml
 import csv
 import re
-from helpers import pretty_print, pretty_format, chinese_weekdays, list_to_comma_separated
+from helpers import pretty_format, list_to_comma_separated
 
 def get_teachers(teachers_text):
     return teachers_text.replace('* ', ',').rstrip(',').rstrip('*').replace(' ', ',')
 
-def get_lessons(week_text, day_text, start_end_text, location):
-    lessons = []
-
+def get_lesson(weeks_text, day_text, start_end_text, location):
     if location == '  ':
         location = None
 
@@ -21,7 +17,7 @@ def get_lessons(week_text, day_text, start_end_text, location):
     else:
         day_text = int(day_text)
 
-    week_num = re.findall("\d{1,2}", week_text)
+    week_num = re.findall("\d{1,2}", weeks_text)
     try:
         weeks = [i for i in range(int(week_num[0]), int(week_num[1]) + 1)]
     except:
@@ -43,16 +39,16 @@ def get_lessons(week_text, day_text, start_end_text, location):
     else:
         number[0] = int(number[0])
 
-    lessons.append({
+    lesson = {
         'day': day_text,
         'start': number[0],
         'end': number[0],
         'weeks': weeks,
-        'weeks_raw': week_text,
+        'weeks_raw': weeks_text,
         'location': location,
-    })
+    }
 
-    return lessons
+    return lesson
 
 source = csv.reader(open('data/bjtu.csv'))
 source.next()
@@ -61,41 +57,40 @@ prev_code_name = '-1'
 
 courses = []
 for row in source:
-	try:
-		test = row[0]
-	except:
-		break
-	teacher = get_teachers(row[6])
-	week_text = row[7]
-	day_text = row[8]
-	start_text = row[9]
-	location = row[10] + ' ' + row[11] + ' ' + row[12]
-	lessons = get_lessons(week_text, day_text, start_text, location)
-	code_name = row[3]
+    try:
+        test = row[0]
+    except:
+        break
+    teacher = get_teachers(row[6])
+    weeks_text = row[7]
+    day_text = row[8]
+    start_text = row[9]
+    location = row[10] + ' ' + row[11] + ' ' + row[12]
+    lesson = get_lesson(weeks_text, day_text, start_text, location)
+    code_name = row[3]
 
-	course = {
-		'original_id': row[1],
-		'name': row[2],
-		'credit': float(row[4]),
-		'teacher': teacher,
-		'lessons': lessons,
-	}
+    course = {
+        'original_id': row[1],
+        'name': row[2],
+        'credit': row[4],
+        'teacher': teacher,
+        'lessons': [lesson],
+    }
 
-	try:
-		last_course = courses.pop()
-	except:
-		pass
-	else:
-	    if course['original_id'] == last_course['original_id'] and course['teacher'] == last_course['teacher'] and prev_code_name == code_name:
-	        course['lessons'] = course['lessons'] + last_course['lessons']
-	    else:
-	        courses.append(last_course)
+    if courses:
+        if (course['original_id'] == courses[-1]['original_id']
+          and course['teacher'] == courses[-1]['teacher']
+          and prev_code_name == code_name):
+            courses[-1]['lessons'].append(lesson)
+        else:
+            courses.append(course)
+    else:
+        courses.append(course)
 
-	prev_code_name = code_name
-	courses.append(course)
+    prev_code_name = code_name
 
-#print courses
+# print courses
 total_courses = len(courses)
-if courses != []:
+if courses:
     with open(('bjtu.yaml'), 'w') as yaml_file:
         yaml_file.write(pretty_format(courses))
